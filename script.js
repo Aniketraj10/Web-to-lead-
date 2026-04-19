d1e8d2e0c0ac3a2b6b93c916d7785a3af48db64c
Aniketraj10 <anipandey0829@gmail.com>
2026-04-19 11:32:34 +0530

    recaptch

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 
-function beforesubmit(){
-     let outputdata = document.querySelector(".outputdata")
+let captchaChecked = false;
+
+function beforesubmit(event){
+     if(captchaChecked){
+          let outputdata = document.querySelector(".outputdata")
       let inputdate = document.querySelector(".inputdate")
      
-      console.log(inputdate.value);// string --> (en_IN)
+      //console.log(inputdate.value);// string --> (en_IN)
      
      let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN")
 
      outputdata.value = formattedDate;
+     }else{
+          alert("Please checked the reCAPTCHA box to submit the Lead")
+          event.preventDefault();
+     }
+     
 }
 
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+
+function capthchasuccess(){
+     captchaChecked = true;
+}
